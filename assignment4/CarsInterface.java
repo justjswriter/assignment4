5d4284604481f7cfdacf20a00b9ae8332e4c7197
justjswriter <111772029+justjswriter@users.noreply.github.com>
2024-03-01 00:18:38 +0600

    Delete assignment4 directory

--- assignment4/CarsInterface.java
+++
@@ -1,9 +0,0 @@
-package com.assignment3;
-
-public interface CarsInterface {
-    public void createCars(Cars car);
-    public void showAllCars();
-    public void showCarsBasedOnID(int id);
-    public void updateCars(int id,String name);
-    public void deleteCars(int id);
-}
